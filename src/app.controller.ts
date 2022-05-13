73084502e515f63b2031229af09d2f5912b8babc
Jose Daniel Parra Gonzalez <jose-paper@hotmail.com>
2022-05-13 13:38:35 -0500

    Params

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,18 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('products/:productId')
+  getProducts(@Param('productId') productId: string): string {
+    return `product ${productId}`;
+  }
+
+  @Get('categories/:id/products/:productId')
+  getCategories(
+    @Param('id') id: string,
+    @Param('productId') productId: string,
+  ) {
+    return `productID: ${productId} / id: ${id}`;
   }
 }
