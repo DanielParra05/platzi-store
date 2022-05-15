21dcaffe294ea77f945446cf17034fbe05aca76c
Jose Daniel Parra Gonzalez <jose-paper@hotmail.com>
2022-05-15 18:07:01 -0500

    Add user module remove unnecessary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CategoriesController } from './products/controllers/categories.controller';
-import { OrdersController } from './controllers/orders/orders.controller';
-import { UsersController } from './controllers/users/users.controller';
-import { CustomersController } from './controllers/customers/customers.controller';
-import { BrandsController } from './controllers/brands/brands.controller';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [UsersModule, ProductsModule],
-  controllers: [
-    AppController,
-    CategoriesController,
-    OrdersController,
-    UsersController,
-    CustomersController,
-    BrandsController,
-  ],
-  providers: [AppService],
+  controllers: [CategoriesController],
+  providers: [],
 })
 export class AppModule {}
